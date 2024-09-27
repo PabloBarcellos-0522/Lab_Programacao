4c0f79c4426fd5af1603aa28d1a8504100d61c42
Pablo Barcellos Soares <74993616+PabloBarcellos-0522@users.noreply.github.com>
2024-09-27 07:58:14 -0300

    Delete Labprog-CC2M directory

--- Labprog-CC2M/1B/Exemplos/Aula4/Dicionarios.py
+++
@@ -1,9 +0,0 @@
-carro = {"marca": "Ford", "modelo": "Mustang", "ano": 1964}
-
-print(carro)
-print(type(carro))
-print(carro['modelo'])
-
-print(len(carro))
-print(carro.keys())
-print(carro.values())
